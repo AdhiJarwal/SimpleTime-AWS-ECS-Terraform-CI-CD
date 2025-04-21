3c47aa8671a6b0831a2a2e796c7171813d885800
adhi-searce <adhiraj.jarwal@searce.com>
2025-04-21 23:49:33 +0530

    Adding Dockerfile and updating CI/CD workflow

--- terraform/resource/variables.tf
+++ terraform/resource/variables.tf
@@ -63,14 +63,3 @@ variable "app_count" {
   type        = number
   default     = 1
 }
-
-variable "s3_bucket" {
-  description = "S3 bucket for Terraform state"
-  type        = string  
-}
-
-variable "dynamodb_table" {
-  description = "DynamoDB table for state locking"
-  type        = string  
-  
-}
